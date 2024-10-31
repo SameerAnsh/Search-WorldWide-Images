727111f49ff97a357de0bae1fdbb0d8bb574c324
SameerAnsh <119520223+SameerAnsh@users.noreply.github.com>
2024-10-31 06:45:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import Theme from "./Theme";
+const App = () => {
+  return <main>
+   <Theme />
+   <Gallery />
+   <SearchForm />
+    </main>;
+};
+export default App;
